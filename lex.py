788f697a554d81f9109be7d8a70483c0b695be9f
denis <dns.aqua.ban@gmail.com>
2022-10-22 19:52:55 +0300

    add lex

---
+++ lex.py
@@ -0,0 +1,73 @@
+import ply.lex as lex
+import sys
+
+tokens = [
+    "BIND",
+    "L_BR",
+    "R_BR",
+    "NONE",
+    "SEPR",
+    "SYMBOL",
+    "STATE", 
+    "DEF_ALPH",
+    "DEF_STATES",
+    "ANY_SYMBOL",
+    "MOVE_LEFT",
+    "MOVE_RIGHT"
+]
+
+t_BIND = r'->'
+t_L_BR = r'\('
+t_R_BR = r'\)'
+t_NONE = r'None'
+t_SEPR = r','
+t_DEF_ALPH = r'Alphabet'
+t_DEF_STATES = r'States'
+t_ANY_SYMBOL = r'_'
+t_MOVE_LEFT = r'L'
+t_MOVE_RIGHT = r'R'
+
+t_ignore = ' \t'
+
+
+def t_SYMBOL(t):
+    r'\'.\''
+    t.value = t.value[1:-1]
+    return t
+
+
+def t_STATE(t):
+    r'\$(\!|[0-9]+)'
+    t.value = t.value[1:]
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
+
+
+def main():
+    if len(sys.argv) > 1:
+        filename = str(sys.argv[1])
+
+        with open(filename, 'r') as fin, open(filename + ".out", "w") as fout:
+            lexer.input("".join(fin.readlines()))
+
+            while True:
+                tok = lexer.token()
+                if not tok:
+                    break
+                print(tok, file=fout)
+
+
+if __name__ == "__main__":
+    main()
